11189994ea3b57430faae1d76efd03c929d2e0d2
Konstantin Simeonov <kon.simeonov@protonmail.com>
2021-12-07 03:32:27 +0200

    it's cuter when the functions are smaller

--- 06-lanternfish/solution.js
+++ 06-lanternfish/solution.js
@@ -1,23 +1,18 @@
 const input0 =  `3,4,3,1,2`
 const input1 = `2,3,1,3,4,4,1,5,2,3,1,1,4,5,5,3,5,5,4,1,2,1,1,1,1,1,1,4,1,1,1,4,1,3,1,4,1,1,4,1,3,4,5,1,1,5,3,4,3,4,1,5,1,3,1,1,1,3,5,3,2,3,1,5,2,2,1,1,4,1,1,2,2,2,2,3,2,1,2,5,4,1,1,1,5,5,3,1,3,2,2,2,5,1,5,2,4,1,1,3,3,5,2,3,1,2,1,5,1,4,3,5,2,1,5,3,4,4,5,3,1,2,4,3,4,1,3,1,1,2,5,4,3,5,3,2,1,4,1,4,4,2,3,1,1,2,1,1,3,3,3,1,1,2,2,1,1,1,5,1,5,1,4,5,1,5,2,4,3,1,1,3,2,2,1,4,3,1,1,1,3,3,3,4,5,2,3,3,1,3,1,4,1,1,1,2,5,1,4,1,2,4,5,4,1,5,1,5,5,1,5,5,2,5,5,1,4,5,1,1,3,2,5,5,5,4,3,2,5,4,1,1,2,4,4,1,1,1,3,2,1,1,2,1,2,2,3,4,5,4,1,4,5,1,1,5,5,1,4,1,4,4,1,5,3,1,4,3,5,3,1,3,1,4,2,4,5,1,4,1,2,4,1,2,5,1,1,5,1,1,3,1,1,2,3,4,2,4,3,1`
-const init = input1.split(`,`).map(Number)
+const fish = eval(`[${input1}]`)
 
-const days = 256
-
-const calc_fish = (n, day, mem = {}) => {
-  const key = `${n}-${day}`
-  if (key in mem) return mem[key]
-  if (n >= day) return BigInt(1)
-
-  return mem[key] = calc_fish(8, day - n - 1, mem) + calc_fish(7, day - n, mem)
+const cached = (fn, mem = {}) => (...args) => {
+  const key = args.join(`-`)
+  return key in mem ? mem[key] : mem[key] = fn(...args)
 }
 
-const mem = {}
-console.log(init.reduce((all, f) => all + calc_fish(f, days, mem), BigInt(0)))
+const calc_fish = cached(
+  (n, day) =>
+    n >= day
+      ? BigInt(1)
+      : calc_fish(8, day - n - 1) + calc_fish(7, day - n)
+)
 
-//4 18
-//  6 13
-//    6 6
-//      6 0
-//    8 5
-//  8 12
+const answer = fish.map(f => calc_fish(f, 256)).reduce((a, b) => a + b)
+console.log(answer)
