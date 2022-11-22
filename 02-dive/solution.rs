16b9b29dbe32cfc31c6595a2ce86080ddb5f5be4
Konstantin Simeonov <kon.simeonov@protonmail.com>
2022-11-22 16:57:02 +0200

    ugly but works i guess

--- 02-dive/solution.rs
+++ 02-dive/solution.rs
@@ -3,17 +3,24 @@ use std::fs;
 fn main() {
     let input = fs::read_to_string("./input")
         .expect("To read the file");
-    let (x, y, aim, y2) = input.lines()
-        .map(|x| match x.split(" ").collect::<Vec<_>>()[..] {
-            ["forward", d] => (d.parse::<i32>().unwrap(), 0),
-            ["down", d] => (0, d.parse::<i32>().unwrap()),
-            ["up", d] => (0, -d.parse::<i32>().unwrap()),
-            _ => (0, 0)
+    let (x, y1, aim, y2) = input.lines()
+        .map(|x| {
+            let (cmd, d) = if let [cmd, delta] = x.split(" ").collect::<Vec<_>>()[..] {
+                (cmd, delta.parse::<i32>().unwrap())
+            } else {
+                ("", 0)
+            };
+            match cmd {
+                "forward" => (d, 0),
+                "down" => (0, d),
+                "up" => (0, -d),
+                _ => (0, 0)
+            }
         })
         .fold(
             (0, 0, 0, 0),
             |(x, y1, aim, y2), (dx, dy)| (x + dx, y1 + dy, aim + dy, y2 + dx * aim)
         );
 
-    println!("part 1: {}, aim: {}, part 2: {}", x * y, aim, x * y2)
+    println!("part 1: {}, aim: {}, part 2: {}", x * y1, aim, x * y2)
 }
