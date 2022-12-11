ec4bed7098faca84bd6aa268d361a0d7f85fbc4f
Konstantin Simeonov <kon.simeonov@protonmail.com>
2022-12-11 03:52:13 +0200

    cute

--- 10/solution.hs
+++ 10/solution.hs
@@ -2,20 +2,23 @@ import System.IO
 import Data.List
 
 main = do
-  input <- lines <$> readFile "./input1"
-  let (_, _, sum) = foldl' step (1, 0, 0) input
-  print $ sum
+  input <- lines <$> readFile "./input"
+  let (_, _, sum, screen) = foldl' step (1, 0, 0, "") input
+  print sum
+  putStrLn $ intercalate "\n" $ chunks 40 $ reverse screen
 
   where
-    step :: (Int, Int, Int) -> String -> (Int, Int, Int)
     step state "noop" = cycle state
     step state add =
-      let [_, value] = words add
-          (x, i, sum) = cycle $ cycle state
-      in (x + read value, i, sum)
+      let (x, i, sum, screen) = cycle $ cycle state
+      in (x + (read $ drop 5 $ add), i, sum, screen)
 
-    cycle :: (Int, Int, Int) -> (Int, Int, Int)
-    cycle (x, i, sum) =
+    cycle (x, i, sum, screen) =
       let newi = i + 1
           newSum = sum + if newi `mod` 40 == 20 then (x * newi) else 0
-      in (x, newi, newSum)
+          spriteDist = abs $ x - (i `mod` 40)
+          newScreen = (if spriteDist <= 1 then '#' else ' '):screen
+      in (x, newi, newSum, newScreen)
+
+    chunks n [] = []
+    chunks n xs = let (chunk, rest) = splitAt n xs in chunk:chunks n rest
