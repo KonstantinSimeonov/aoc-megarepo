e6851a88c29845dae0e7ca30704feb8a00f41709
Konstantin Simeonov <kon.simeonov@protonmail.com>
2022-12-17 18:53:20 +0200

    im gonna regret this

--- 16/solution.rs
+++ 16/solution.rs
@@ -1,10 +1,24 @@
+use std::cmp;
 use std::collections::{HashMap, HashSet, VecDeque};
 use std::fs;
-use std::cmp;
 
 fn main() {
     let input = fs::read_to_string("./input").expect("pipezzz boii");
 
+    let mut cnt = 1;
+    let mut pipes_names = HashMap::new();
+    let mut add_key = |name: &str| -> i32 {
+        let k = if !pipes_names.contains_key(name) {
+            let old = cnt;
+            cnt *= 2;
+            pipes_names.insert(name.to_string(), old);
+            old
+        } else {
+            *pipes_names.get(name).unwrap()
+        };
+
+        k
+    };
     let ps = input
         .lines()
         .map(|line| {
@@ -23,68 +37,117 @@ fn main() {
         })
         .collect::<HashMap<_, _>>();
 
-    let graph = ps
+    let mut ks = ps
         .iter()
-        .filter_map(|(k, (rate, _))| if *rate == 0 && k != &"AA" { None } else { Some(*k) })
-        .map(|k| (k, neighbs(&ps, k)))
+        .filter_map(|(k, (rate, _))| {
+            if *rate == 0 && *k != "AA" {
+                None
+            } else {
+                Some(*k)
+            }
+        }).collect::<Vec<_>>();
+
+    ks.sort();
+
+    let start = 1;
+    let graph = ks.iter()
+        .map(|k| {
+            let (rate, ns) = neighbs(&ps, k);
+            add_key(k);
+            let keyed = ns
+                .iter()
+                .map(|(name, dist)| (add_key(name), *dist))
+                .collect::<Vec<(i32, i32)>>();
+
+            (add_key(k), (rate, keyed))
+        })
         .collect::<HashMap<_, _>>();
 
-    println!("{}, {:?}", graph.len(), graph);
-    println!("{}", max_release_nice(&graph, "AA", 30, &mut HashSet::new(), &mut HashMap::new()));
+    //println!("{:?}", {
+    //    let mut ks = graph.keys().collect::<Vec<_>>();
+    //    ks.sort();
+    //    ks
+    //});
 
+    //println!("{}", graph.iter().map(|(k, v)| "" + k.to_string() + " -> " + v.to_string()));
+
+    println!("{}, {:?}", graph.len(), {
+        let mut xs = graph.iter().map(|(k, v)| {
+            let mut v1 = v.1.clone();
+            v1.sort();
+            (k, (v.0, v1))
+        }).collect::<Vec<_>>();
+        xs.sort();
+        xs
+    });
+    println!(
+        "{}",
+        max_release_nice(&graph, start, 30, 0, &mut HashMap::new())
+    );
+}
+
+fn insert(t: i32, c: i32) -> i32 {
+    assert!(t < 1 << 16);
+    assert!(c < (1 << 16));
+    assert!(t & c == 0);
+    t | c
+}
+
+fn has(t: i32, c: i32) -> bool {
+    assert!(t < 1 << 16);
+    assert!(c < (1 << 16));
+    t & c != 0
 }
 
 // (1600..1626)
 fn max_release_nice<'a>(
-    net: &HashMap<&'a str, (i32, Vec<(&'a str, i32)>)>,
-    current: &'a str,
+    net: &HashMap<i32, (i32, Vec<(i32, i32)>)>,
+    current: i32,
     left: i32,
-    turned: &mut HashSet<&'a str>,
-    cache: &mut HashMap<(i32, String, &'a str), i32>
+    turned: i32,
+    cache: &mut HashMap<(i32, i32, i32), i32>,
 ) -> i32 {
-    let key = turned.iter().map(|s| s.to_string()).collect::<String>();
-    if let Some(res) = cache.get(&(left, key.clone(), current)) {
-        return *res
+    if let Some(res) = cache.get(&(left, turned, current)) {
+        return *res;
     }
 
-    if left <= 0 || turned.len() == net.len() {
-        return 0
+    if left <= 0 {
+        return 0;
     }
 
-    let (rate, next) = net.get(current).unwrap();
+    let (rate, next) = net.get(&current).unwrap();
 
-    let with = if turned.contains(current) {
+    let with = if has(turned, current) {
         0
     } else {
-        turned.insert(current);
         let l = left - 1;
         let p = rate * l;
         let press = next
             .iter()
-            .map(|(name, cost)| if turned.contains(name) { 0 } else { max_release_nice(net, name, l - cost, turned, cache) })
+            .map(|(name, cost)| {
+                max_release_nice(net, *name, l - cost, insert(turned, current), cache)
+            })
             .max()
             .unwrap();
-
-        turned.remove(current);
 
         p + press
     };
 
     let without = next
         .iter()
-        .map(|(name, cost)| if turned.contains(name) { 0 } else { max_release_nice(net, name, left - cost, turned, cache) })
+        .map(|(name, cost)| max_release_nice(net, *name, left - cost, turned, cache))
         .max()
         .unwrap();
 
     let res = cmp::max(with, without);
-    cache.insert((left, key, current), res);
+    cache.insert((left, turned, current), res);
 
     res
 }
 
 fn neighbs<'a>(
-    net: &HashMap<&str, (i32, Vec<&'a str>)>,
-    current: &'a str
+    net: &HashMap<&'a str, (i32, Vec<&'a str>)>,
+    current: &'a str,
 ) -> (i32, Vec<(&'a str, i32)>) {
     let mut visited = HashSet::new();
     let mut nodes = VecDeque::from([(current, 0)]);
@@ -109,7 +172,6 @@ fn neighbs<'a>(
 
     (*rate, result)
 }
-
 
 // slow af
 fn max_release<'a>(
