53929187f0f8ac20f7bf9713008c92e1ddcfe16c
Konstantin Simeonov <kon.simeonov@protonmail.com>
2022-12-27 09:43:11 +0200

    oneshot; part 2 looks terrible

---
+++ 22/solution.rs
@@ -0,0 +1,119 @@
+use std::fs;
+
+static DELTAS: &[(i32, i32); 4] = &[
+    (0, 1),
+    (1, 0),
+    (0, -1),
+    (-1, 0)
+];
+
+fn parse_dirs(dirs: &str) -> Vec<(i32, i32)> {
+    let mut res = Vec::new();
+    let mut dir = 0;
+    let mut forward = 0;
+
+    for &x in dirs.as_bytes() {
+        if x == b'R' {
+            res.push((forward, dir));
+            forward = 0;
+            dir = 1;
+        } else if x == b'L' {
+            res.push((forward, dir));
+            forward = 0;
+            dir = -1;
+        } else {
+            forward = forward * 10 + (x - 48) as i32;
+        }
+    }
+
+    res.push((forward, dir));
+
+    res
+}
+
+fn index(y: i32, x: i32, board: &Vec<&[u8]>) -> Option<u8> {
+    if y < 0 || x < 0 {
+        return None;
+    }
+
+    let sy = y as usize;
+    let sx = x as usize;
+
+    if sy >= board.len() || sx >= board[sy].len() {
+        return None;
+    }
+
+    Some(board[sy][sx])
+}
+
+fn main() {
+    let input = fs::read_to_string("./input").expect("gadno maze");
+
+    let (raw_board, raw_dirs) = input.trim_end().split_once("\n\n").unwrap();
+
+    let board = raw_board.lines().map(|line| line.as_bytes()).collect::<Vec<_>>();
+
+    let dirs = parse_dirs(raw_dirs);
+    let mut current = board[0]
+        .iter()
+        .enumerate()
+        .find_map(|(i, &tile)| if tile == b'.' { Some((0, i as i32)) } else { None })
+        .unwrap();
+
+    println!("start {:?}", current);
+
+    let mut dir = 0;
+
+    let dl = DELTAS.len() as i32;
+
+    for &(forward, turn) in dirs.iter() {
+        dir = (dl + dir + turn) % dl;
+        let (dy, dx) = DELTAS[dir as usize];
+
+        let mut f = forward;
+        while f > 0 {
+            let ny = current.0 + dy;
+            let nx = current.1 + dx;
+
+            //println!("{:?}", current);
+
+            match index(ny, nx, &board) {
+                Some(b'.') => current = (ny, nx),
+                Some(b'#') => break,
+                Some(b' ') | None => {
+                    let rdy = dy * -1;
+                    let rdx = dx * -1;
+
+                    let mut wy = current.0 + rdy;
+                    let mut wx = current.1 + rdx;
+
+                    while let Some(c) = index(wy, wx, &board) {
+                        if c == b' ' {
+                            break;
+                        }
+
+                        wy += rdy;
+                        wx += rdx;
+                    }
+
+                    wy -= rdy;
+                    wx -= rdx;
+
+                    if let Some(c) = index(wy, wx, &board) {
+                        if c == b'.' {
+                            current = (wy, wx);
+                        }
+                    }
+                },
+                _ => unreachable!()
+            }
+
+            f -= 1;
+        }
+    }
+
+    println!("current {:?}, dir: {}", current, dir);
+
+    let part1 = (current.0 + 1) * 1000 + (current.1 + 1) * 4 + dir;
+    println!("part1: {}", part1)
+}
