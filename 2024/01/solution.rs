5f5addde81d98d1c1c19a3aabb7d61fcbe60d58a
Konstantin Simeonov <kon.simeonov@protonmail.com>
2024-12-01 12:52:54 +0200

    day 1

---
+++ 2024/01/solution.rs
@@ -0,0 +1,38 @@
+use std::fs;
+use std::iter;
+use std::collections::HashMap;
+
+fn solve(input: String) -> (i32, i32) {
+  let (mut fst, mut snd): (Vec<_>, Vec<_>) = input
+    .trim()
+    .split("\n")
+    .map(|line| {
+      let (l, r) = line.split_once("   ").unwrap();
+      (l.parse::<i32>().unwrap(), r.parse::<i32>().unwrap())
+    }).unzip();
+
+  fst.sort();
+  snd.sort();
+
+  let freq = snd.iter().fold(
+    HashMap::<i32, i32>::new(),
+    |mut f, &x| {
+      *f.entry(x).or_default() += 1;
+      f
+    }
+  );
+
+  let a2: i32 = fst.iter().map(|x| freq.get(x).unwrap_or(&0) * x).sum();
+
+  let a1 = iter::zip(fst, snd).map(|(a, b)| (a - b).abs()).sum();
+
+  (a1, a2)
+}
+
+fn main() {
+  let input0 = fs::read_to_string("./input0").expect("stuff");
+  let input = fs::read_to_string("./input").expect("stuff");
+
+  println!("{:?}", solve(input0));
+  println!("{:?}", solve(input));
+}
