bf7e67ec45a3cc873e4bbc959130cf26044cb36d
Konstantin Simeonov <kon.simeonov@protonmail.com>
2025-09-17 15:11:52 +0300

    a little fun with bash

---
+++ 2024/01/main.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+set -eo pipefail
+
+input_file=input
+
+mapfile -t xs < <(cut -d ' ' -f 1 < "$input_file" | sort --numeric-sort)
+mapfile -t ys < <(cut -d ' ' -f 4 < "$input_file" | sort --numeric-sort)
+
+declare -i len="${#xs[@]}"
+declare -i total=0
+for ((i = 0; i<len; i += 1)); do
+  diff=$((xs[i] - ys[i]))
+  abs=$(( diff < 0 ? -diff : diff))
+  total=$(( total + abs ))
+done
+
+echo "Part 1: $total"
+
+declare -A ys_freqs
+for y in "${ys[@]}"; do
+  if [[ -v ys_freqs["$y"] ]]; then
+    (( ys_freqs[$y]++ ))
+    echo "1"
+  else
+    ys_freqs[$y]=1
+  fi
+done
+
+declare -i part2=0
+for x in "${xs[@]}"; do
+  part2=$(( part2 + x * ys_freqs[$x] ))
+done
+
+echo "Part 2: $part2"
